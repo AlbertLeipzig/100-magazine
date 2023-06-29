4219fdcec4f509fb7bb9ae5235762a1f240021c2
AlbertLeipzig <albertclemvill@gmail.com>
2023-06-29 07:47:48 +0200

    finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,52 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink, useRoutes } from 'react-router-dom';
 import { Home } from '../pages/Home.jsx';
 import { About } from '../pages/About.jsx';
 import { Careers } from '../pages/Careers.jsx';
 import { Location } from '../pages/Location.jsx';
 export const Header = () => {
+
+  const routes = [
+    {
+      path: '/',
+      element: <Home />,
+      title: 'Home',
+    },
+    {
+      path: 'about',
+      element: <About />,
+      title: 'About',
+    },
+    {
+      path: 'careers',
+      element: <Careers />,
+      title: 'Careers',
+    },
+    {
+      path: 'location',
+      element: <Location />,
+      title: 'Location',
+    },
+  ];
+  const pageTitle = useRoutes(routes);
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="careers" element={<Careers />} />
+    <Route path="location" element={<Location />} />
+  </Routes>;
   return (
     <header>
       <div>
-        <h2>Title</h2>
+        <h2>scoot</h2>
         <nav>
           <NavLink to="/">Home</NavLink>
           <NavLink to="about">About</NavLink>
           <NavLink to="careers">Careers</NavLink>
           <NavLink to="location">Location</NavLink>
         </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="careers" element={<Careers />} />
-          <Route path="location" element={<Location />} />
-        </Routes>
+        <button>Get Scootin</button>
       </div>
+      <h1>{pageTitle}</h1>
     </header>
   );
 };
