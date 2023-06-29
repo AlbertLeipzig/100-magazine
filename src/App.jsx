263ac64ec38c228ae8c1e5c0ea28d4f070ecd29a
AlbertLeipzig <albertclemvill@gmail.com>
2023-06-29 08:13:44 +0200

    added footer and assets for google and apple store as well as social media

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from './components/Header';
-
+import { Footer } from './components/Footer';
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import { NavLink } from 'react-router-dom';
+
+import appleStoreIcon from '../../public/assets/apple-store-icon.png';
+import googleStoreIcon from '../../public/assets/google-store-icon.png';
+import facebookIcon from '../../public/assets/facebook-icon.png';
+import instagramIcon from '../../public/assets/instagram-icon.png';
+import twitterIcon from '../../public/assets/twitter-icon.png';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h2>Sign up and Scoot off today</h2>
+        <button>
+          <a href="https://www.apple.com/de/app-store/" target="_blank">
+            <img src={appleStoreIcon} alt="apple store icon" />
+          </a>
+        </button>
+        <button>
+          <a href="https://store.google.com/?pli=1&hl=de" target="_blank">
+            <img src={googleStoreIcon} alt="google store icon" />
+          </a>
+        </button>
+      </div>
+      <div>
+        <h2>scoot</h2>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="careers">Careers</NavLink>
+          <NavLink to="location">Location</NavLink>
+        </nav>
+        <menu>
+          <a href="https://www.facebook.com/" target="_blank">
+            <img src={facebookIcon} alt="facebook icon" />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank">
+            <img src={instagramIcon} alt="instagram icon" />
+          </a>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="twitter icon" />
+          </a>
+        </menu>
+      </div>
+    </footer>
+  );
+};
