83f4fa202de6e02401e4eb359f9b81e4d8a6bcd9
AlbertLeipzig <albertclemvill@gmail.com>
2023-06-29 07:29:18 +0200

    header working on App.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './Home';
-import { About } from './About';
-import { Careers } from './Careers';
-import { Location } from './Location';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { Home } from '../pages/Home.jsx';
+import { About } from '../pages/About.jsx';
+import { Careers } from '../pages/Careers.jsx';
+import { Location } from '../pages/Location.jsx';
 export const Header = () => {
   return (
     <header>
       <div>
         <h2>Title</h2>
         <nav>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="careers" element={<Careers />} />
-            <Route path="location" element={<Location />} />
-          </Routes>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="careers">Careers</NavLink>
+          <NavLink to="location">Location</NavLink>
         </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="careers" element={<Careers />} />
+          <Route path="location" element={<Location />} />
+        </Routes>
       </div>
     </header>
   );
